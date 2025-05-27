ff55157403165b92ef220c551581438d54069011
Lucas <oliveira.lucas23@edu.pucrs.br>
2025-05-26 21:23:57 -0300

    implementacao inicial

---
+++ PatternMatching.java
@@ -0,0 +1,35 @@
+public class PatternMatching {
+    public static int findFirstOccurrence(String s1, String s2, int[] iteracoes, int[] instrucoes) {
+        int n = s1.length();
+        int m = s2.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            instrucoes[0]++;
+            boolean match = true;
+            for (int j = 0; j < m; j++) {
+                instrucoes[0]++;
+                iteracoes[0]++;
+                if (s1.charAt(i + j) != s2.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ABCDCBDCBDACBDABDCBADF";
+        String s2 = "ADF";
+        int[] iteracoes = { 0 };
+        int[] instrucoes = { 0 };
+
+        int pos = findFirstOccurrence(s1, s2, iteracoes, instrucoes);
+        System.out.println("Posição: " + pos);
+        System.out.println("Iterações: " + iteracoes[0]);
+        System.out.println("Instruções: " + instrucoes[0]);
+    }
+}
